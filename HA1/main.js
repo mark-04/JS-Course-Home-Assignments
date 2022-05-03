a6f1902abf3821723a42d37b622c5f8833893f49
Mark <blusuta@gmail.com>
2022-05-03 17:05:22 +0300

    Initial

---
+++ HA1/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const getInputs  = () => [prompt(), prompt()]; 
+const coerceEach = (coercingFn, array) => array.map(coercingFn);
+const allValid   = (array) => (
+  array
+    .map((value) => !(Number.isNaN(value)))
+    .reduce((fst, snd) => fst && snd, true)
+);
+
+const numberWithRadix = (number, radix) => Number(number.toString(radix));
+const sumAndDivide    = (first, second) => `Ответ: ${first + second}, ${first / second}.`;
+
+const main = (computation) => {
+  const inputs = coerceEach(Number, getInputs());
+
+  if (allValid(inputs)) {
+    let result = computation(...inputs);
+    console.log(result);
+  } else {
+    console.log('Некорректный ввод!')
+  }
+};
